5cd4bc1dc31c462df01d8c9012cbe142f34da1e6
boyi.zhang <boyi.zhang@shopee.com>
2021-01-25 20:21:02 +0800

    1

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
 
 if __name__ == '__main__':
     print("boyi")
-    print("hkhaldlkkka")
-    print("beoyi")
-    print("test")
-    print("test2")
-    print("test2")
-    print("test3")
-    print("test")
-    print("test")
-    print("test")
-    print("test")
-    print("test")
-    print("test")
-    print("test2")
-    print("test3")
-    print("test")
-    print("boyi")
-    print("test")
-    print("test")
-    print('test2')
-    print("boyi")
+
+
